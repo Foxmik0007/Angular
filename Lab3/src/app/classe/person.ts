39d2c5233c24b18e037d2b230e2723202f0b7395
Foxmik0007 <mrafa099@uottawa.ca>
2021-10-01 16:57:20 -0300

    50%

---
+++ Lab3/src/app/classe/person.ts
@@ -0,0 +1,14 @@
+export class Person {
+    public lastName: string;
+    public firstName: string;
+    public cellNumber: string;
+    public email: string;
+
+    constructor(lastName: string, firstName: string, cellNumber: string, email: string){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.cellNumber = cellNumber;
+        this.email = email;
+    }
+
+}
