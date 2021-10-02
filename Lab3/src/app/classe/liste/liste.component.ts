51e208a914b0ab2f76775900a34b51a488e60036
Mika Rafaralahy <mrafa099@uottawa.ca>
2021-10-01 21:14:38 -0500

    Working

--- Lab3/src/app/classe/liste/liste.component.ts
+++ Lab3/src/app/classe/liste/liste.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Person } from '../person';
 
 @Component({
@@ -7,6 +7,7 @@ import { Person } from '../person';
   styleUrls: ['./liste.component.css']
 })
 export class ListeComponent implements OnInit {
+
 
   liste:Person[] = [];
 
@@ -22,26 +23,38 @@ export class ListeComponent implements OnInit {
     this.liste = [this.Mika, this.Maeva]
   }
 
-  Maeva: Person = new Person("Maeva", "Dupuis", "0332243412", "Maeva@gmail.com");
-  Mika: Person = new Person("Mika", "Rafaralahy", "0327689876", "Rafaralahymika@gmail.com");
+  Maeva: Person = new Person("Maeva", "Dupuis", "0332243412", "Maeva@gmail.com")
+  Mika: Person = new Person("Mika", "Rafaralahy", "0327689876", "Rafaralahymika@gmail.com")
   
   // Suppression
   suppression(id: number){
     this.liste = this.liste.filter((v, i) => i !==id );
   }
 
-  ajouter(){
-    this.liste.push({
-      lastName: this.inputLastName,
-      firstName: this.inputFirstName,
-      cellNumber: this.inputCellNumber,
-      email: this.inputEmail
-    })
-
-    this.inputLastName ="";
-    this.inputFirstName="";
-    this.inputCellNumber="";
-    this.inputEmail="";
+  ajouter() {
+    
+    if (this.inputFirstName == "") {
+      alert("Please, enter your firstname")
+    } else if (this.inputLastName == "") {
+      alert("Please, enter your lastname")
+    } else if ((this.inputCellNumber.charAt(0) != "0" || this.inputCellNumber.length != 10 || this.inputCellNumber.charAt(3) != "0") && this.inputCellNumber != "") {
+      alert("Please, review your cell number")
+    } else if ((!this.inputEmail.includes("@" || this.inputEmail.length < 5) && this.inputEmail != "")) {
+      alert("Please, review your email")
+    } else {
+      this.liste.push({
+        lastName: this.inputLastName,
+        firstName: this.inputFirstName,
+        cellNumber: this.inputCellNumber,
+        email: this.inputEmail
+      })
+  
+      this.inputLastName ="";
+      this.inputFirstName="";
+      this.inputCellNumber="";
+      this.inputEmail="";
+    }
+    
   }
   
 }
--- Lab3/src/app/classe/person.ts
+++ Lab3/src/app/classe/person.ts
@@ -1,10 +1,10 @@
 export class Person {
     public lastName: string;
     public firstName: string;
-    public cellNumber: string;
-    public email: string;
+    public cellNumber?: string;
+    public email?: string;
 
-    constructor(lastName: string, firstName: string, cellNumber: string, email: string){
+    constructor(lastName: string, firstName: string, cellNumber?: string, email?: string){
         this.lastName = lastName;
         this.firstName = firstName;
         this.cellNumber = cellNumber;
